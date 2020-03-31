47108dddcb7d8d088ff102a642d7165671d10506
gabrielpieptea <gabriel.pieptea@gmail.com>
2020-03-31 19:19:32 +0300

    v0.5

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var map;
 
+
+// plane path design
 var dottedLine = {
   path: "M 0,-1 0,1",
   strokeOpacity: 1,
   scale: 2
 };
 
+/* ---------------------------------
+plane animation along its path
+-----------------------------------*/
 function planeMotion(line) {
   var count = 0;
   window.setInterval(function() {
@@ -16,13 +21,18 @@ function planeMotion(line) {
   }, 20);
 }
 
-//----------------
+/* ---------------------------------
+map initialization
+-----------------------------------*/
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 51.55, lng: 4.511 },
     zoom: 8
   });
 
+  /* ---------------------------------
+all markers defined
+-----------------------------------*/
   var marker = airportList =>
     new google.maps.Marker({
       position: airportList,
@@ -36,12 +46,19 @@ function initMap() {
       )
     });
 
-    var circleSymbol = {
-      path: google.maps.SymbolPath.CIRCLE,
-      strokeOpacity: '1'
-    };
-    
-  
+    for (let i = 0; i < airportList.length; i++) {
+      marker(airportList[i]).setMap(map);
+    }
+
+  var circleSymbol = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: "1"
+  };
+
+
+  /* ---------------------------------
+all flight path defined
+-----------------------------------*/
   var flightPath = (departureAirport, arrivalAirport) =>
     new google.maps.Polyline({
       path: [departureAirport, arrivalAirport],
@@ -66,9 +83,7 @@ function initMap() {
     planeMotion(flightPath(flightIndex[i].x, flightIndex[i].y));
   }
 
-  for (let i = 0; i < airportList.length; i++) {
-    marker(airportList[i]).setMap(map);
-  }
+  
 
   /* ---------------------------------
 defining sectors + event handling
@@ -93,6 +108,57 @@ defining sectors + event handling
   });
   areaTwoPolygon.setMap(map);
 
+ 
+  /* ---------------------------------
+action buttons for methods
+-----------------------------------*/
+  const hoverButt = document.getElementById("hoverArea");
+  const resetButt = document.getElementById("resetArea");
+  const nightButt = document.getElementById("nightMode");
+
+  hoverButt.setAttribute("style", "background-color: red");
+  hoverButt.addEventListener("click", hovertheArea);
+  resetButt.addEventListener("click", e => initMap());
+  nightButt.addEventListener("click", e => toggleDarkMode());
+  nightButt.setAttribute("style", "background-color: red");
+
+  
+  /* ---------------------------------
+map night mode
+-----------------------------------*/
+  let darkModeBool = false;
+  function toggleDarkMode() {
+    if (darkModeBool) {
+      map.setOptions({ styles: "" });
+      darkModeBool = false;
+      nightButt.setAttribute("style", "background-color: red");
+    } else {
+      map.setOptions({ styles: darkMode });
+      darkModeBool = true;
+      nightButt.setAttribute("style", "background-color: green");
+    }
+  }
+
+
+  /* ---------------------------------
+map hover area for sectors
+-----------------------------------*/
+  let cond = true;
+  function hovertheArea() {
+    if (cond) {
+      areaOnePolygon.setOptions({ fillOpacity: 0.35 });
+      areaTwoPolygon.setOptions({ fillOpacity: 0.35 });
+      cond = false;
+    } else {
+      areaOnePolygon.setOptions({ fillOpacity: 0 });
+      areaTwoPolygon.setOptions({ fillOpacity: 0 });
+      cond = true;
+    }
+  }
+
+
+  //map sectors hover - mouseover
+  /*
   google.maps.event.addListener(areaOnePolygon, "mouseover", function() {
     areaOnePolygon.setOptions({ fillOpacity: 0.35 });
   });
@@ -105,4 +171,5 @@ defining sectors + event handling
   google.maps.event.addListener(areaTwoPolygon, "mouseout", function() {
     areaTwoPolygon.setOptions({ fillOpacity: 0 });
   });
+*/
 }
