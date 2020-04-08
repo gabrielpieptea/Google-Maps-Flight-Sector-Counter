455d812ae651ead2360d642b67d631aa20cc8d8e
gabrielpieptea <gabriel.pieptea@gmail.com>
2020-04-08 19:07:44 +0300

    working version of flight counter

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-
-
 // plane path design
 var dottedLine = {
   path: "M 0,-1 0,1",
   strokeOpacity: 1,
   scale: 2,
 };
-
-/* ---------------------------------
-plane animation along its path
------------------------------------*/
-function planeMotion(line) {
-  var count = 0;
-  window.setInterval(function () {
-    count = (count + 1) % 1000;
-    var icons = line.get("icons");
-    icons[1].offset = count / 4 + "%";
-    line.set("icons", icons);
-
-    console.log(count);
-  }, 20);
-}
 
 /* ---------------------------------
 map initialization
@@ -76,17 +59,16 @@ all flight path defined
       ],
       map: map,
     });
-  
+
   for (let i = 0; i < flightIndex.length; i++) {
     allFlights.push({
-      "dep": flightIndex[i].x,
-      "des": flightIndex[i].y,
-      "count": 0,
-      "line": flightPath(flightIndex[i].x, flightIndex[i].y),
-      "inSector": false
+      dep: flightIndex[i].x,
+      des: flightIndex[i].y,
+      count: 0,
+      line: flightPath(flightIndex[i].x, flightIndex[i].y),
+      inSector: false,
     });
   }
-
 
   /* ---------------------------------
 defining sectors + event handling
@@ -109,6 +91,7 @@ defining sectors + event handling
     strokeWeight: 2,
     fillColor: "rgba(255,255,255,0.5);",
     fillOpacity: 0,
+    clickable: false
   });
   areaTwoPolygon.setMap(map);
 
@@ -157,112 +140,46 @@ map hover area for sectors
     }
   }
 
-  //map sectors hover - mouseover
-  /*
-  google.maps.event.addListener(areaOnePolygon, "mouseover", function() {
-    areaOnePolygon.setOptions({ fillOpacity: 0.35 });
-  });
-  google.maps.event.addListener(areaOnePolygon, "mouseout", function() {
-    areaOnePolygon.setOptions({ fillOpacity: 0 });
-  });
-  google.maps.event.addListener(areaTwoPolygon, "mouseover", function() {
-    areaTwoPolygon.setOptions({ fillOpacity: 0.35 });
-  });
-  google.maps.event.addListener(areaTwoPolygon, "mouseout", function() {
-    areaTwoPolygon.setOptions({ fillOpacity: 0 });
-  });
-*/
 
-  punctu = new google.maps.LatLng({ lat: -34, lng: 151 });
+  /* ---------------------------------
+plane animation along its path
+-----------------------------------*/
 
-  google.maps.event.addListener(map, "click", function (event) {
-    alert(google.maps.geometry.poly.containsLocation(punctu, areaOnePolygon));
-  });
-
-  // Animation Test by AndreiU
-  window.setInterval(function() {
+  window.setInterval(function () {
     for (let i = 0; i < flightIndex.length; i++) {
       allFlights[i].count = (allFlights[i].count + 0.5) % 1000;
       var icons = allFlights[i].line.get("icons");
       icons[1].offset = allFlights[i].count / 10 + "%";
       allFlights[i].line.set("icons", icons);
-    
-      // verifica pls toata partea asta
 
-      // Compute coordinates based on offset
-/*
-      var lat1 = allFlights[i].dep.lat;
-      var lat2 = allFlights[i].des.lat;
-      var lon1 = allFlights[i].dep.lng;
-      var lon2 = allFlights[i].des.lng;
-      
-      var R = 6371e3; // metres
-      var φ1 = lat1 * Math.PI/180;
-      var φ2 = lat2 * Math.PI/180;
-      var λ1 = lon1 * Math.PI/180
-      var λ2 = lon2 * Math.PI/180
-      var Δφ = (φ2-φ1);
-      var Δλ = (λ2-λ1) * Math.PI/180;
+      var a = new google.maps.LatLng(
+        allFlights[i].dep.lat,
+        allFlights[i].dep.lng
+      );
+      var b = new google.maps.LatLng(
+        allFlights[i].des.lat,
+        allFlights[i].des.lng
+      );
 
-      var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ/2) * Math.sin(Δλ/2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var e = google.maps.geometry.spherical.interpolate(
+        a,
+        b,
+        allFlights[0].count / 1000
+      ); 
 
-      var D = R * c;
-
-      // Compute initial bearing
-      var y = Math.sin(λ2-λ1) * Math.cos(φ2);
-      var x = Math.cos(φ1)*Math.sin(φ2) -
-      Math.sin(φ1)*Math.cos(φ2)*Math.cos(λ2-λ1);
-      var brng = Math.atan2(y, x) * 180 / Math.PI;
-      
-      d = allFlights[i].count / 1000 * D;
-      // Calculate final point based on initial bearing and 
-      var φ3 = Math.asin( Math.sin(φ1)*Math.cos(d/R) +
-                    Math.cos(φ1)*Math.sin(d/R)*Math.cos(brng) );
-      var λ3 = λ1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(φ1),
-                         Math.cos(d/R)-Math.sin(φ1)*Math.sin(φ3));
-
-
-      var lat3 = φ3 * 180 / Math.PI;
-      var lon3 = λ3 * 180 / Math.PI;
-*/
-      // debugger;
-
-      // verifica intrare sau iesir din sector.
-      
-      var a = new google.maps.LatLng(allFlights[i].dep.lat, allFlights[i].dep.lng);
-      var b = new google.maps.LatLng(allFlights[i].des.lat, allFlights[i].des.lng);
-
-      var e = google.maps.geometry.spherical.interpolate(a, b, (allFlights[0].count / 1000));//(count / 200));
-      
-      
-      // console.log("e = ");
-      // console.log(e);
-
-      // var newMarker = (e) =>
-      // new google.maps.Marker({
-      // position: e,
-      // map: map
-      // });
-
-      //newMarker(e).setMap(map);
-      //var e = new google.maps.LatLng(lat3, lon3);
-      //var e = new google.maps.LatLng(0, 45);
-      
-      var newInSector = google.maps.geometry.poly.containsLocation(e, areaOnePolygon) || google.maps.geometry.poly.containsLocation(e, areaTwoPolygon);
-      if (allFlights[i].inSector && !newInSector)
-        acftInSector--;
-      else if (!allFlights[i].inSector && newInSector)
-        acftInSector++;
+      /* ---------------------------------
+verifies if plane icon is contained in the two sectors
+-----------------------------------*/
+      var newInSector =
+        google.maps.geometry.poly.containsLocation(e, areaOnePolygon) ||
+        google.maps.geometry.poly.containsLocation(e, areaTwoPolygon);
+      if (allFlights[i].inSector && !newInSector) acftInSector--;
+      else if (!allFlights[i].inSector && newInSector) acftInSector++;
 
       allFlights[i].inSector = newInSector;
     }
 
     // Update value
-  document.getElementById("flightno").innerHTML = acftInSector;
+    document.getElementById("flightno").innerHTML = acftInSector;
   }, 20);
-
-  
 }
